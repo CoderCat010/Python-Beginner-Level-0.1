a7f2d53d61e67cbd4ad0c8c7ae09b842794b9c82
CoderCat010 <nusraumme@gmail.com>
2025-06-27 13:38:05 +0600

    added Comparison Operators practices

--- practice-3.py
+++ practice-3.py
@@ -1,5 +1,6 @@
 # Arithmetic Operators
 
+'''
 x = 5
 y = 6
 
@@ -10,3 +11,14 @@ print(x/y)
 print(x//y)
 print(x%y)
 print(3**3)
+'''
+
+# Comparison Operators
+x = 5
+y = 10 
+z = 100
+
+print(x==y==z)
+print(x!=y!=z)
+print(("no! x is less than y") ,x>=y)
+print(x<y<z)
