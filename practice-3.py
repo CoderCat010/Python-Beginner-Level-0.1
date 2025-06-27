3bb6b384b4d3f4cd022d5999728ed8568600a767
CoderCat010 <nusraumme@gmail.com>
2025-06-27 13:55:27 +0600

    added final practices

--- practice-3.py
+++ practice-3.py
@@ -14,6 +14,7 @@ print(3**3)
 '''
 
 # Comparison Operators
+'''
 x = 5
 y = 10 
 z = 100
@@ -22,3 +23,25 @@ print(x==y==z)
 print(x!=y!=z)
 print(("no! x is less than y") ,x>=y)
 print(x<y<z)
+'''
+
+# Assignment Operators
+
+'''
+x = 5
+print("step 1-" , x)
+x += 5
+print("step 2-" , x)
+x *= 10 
+print("step 3-" , x)
+x /= 100
+print("step 4-" , x)
+x //= 8
+print("step 5-" , x)
+x **= 9
+print("step 6-" , x)
+x %= 3
+print("step final-" , x)
+'''
+
+
