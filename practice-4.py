c9070b567b412670dbc24045d59661597aa16eaf
CoderCat010 <nusraumme@gmail.com>
2025-06-27 15:32:07 +0600

    aaded

---
+++ practice-4.py
@@ -0,0 +1,20 @@
+# Conditional Statements
+
+'''PROBLEM NUMBER 1'''
+
+number = 20
+
+if number <= 10:
+    print('yes true!')
+else:
+    print('no! try again')
+
+
+'''PROBLEM NUMBER 2'''
+
+inputNumber = input('Enter your number: ')
+
+if inputNumber % 2 == 0:
+    print('even')
+else: 
+    print('odd')
