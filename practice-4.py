7e0dd0f3360cd2a78ed42c14ba651cffe44ec723
CoderCat010 <nusraumme@gmail.com>
2025-06-27 17:51:24 +0600

    added practice tasks

--- practice-4.py
+++ practice-4.py
@@ -2,19 +2,62 @@
 
 '''PROBLEM NUMBER 1'''
 
-number = 20
+# number = 20
 
-if number <= 10:
-    print('yes true!')
-else:
-    print('no! try again')
+# if number <= 10:
+#     print('yes true!')
+# else:
+#     print('no! try again')
 
 
 '''PROBLEM NUMBER 2'''
 
-inputNumber = input('Enter your number: ')
+# inputNumber = int(input('Enter your number: '))
 
-if inputNumber % 2 == 0:
-    print('even')
-else: 
-    print('odd')
+# # if inputNumber % 2 == 0:
+# #     print('even')
+# # else: 
+# #     print('odd')
+
+
+'''PROBLEM NUMBER 3'''
+
+# mynumbers = 7
+
+# if mynumbers > 0:
+#     print('Your number is postive')
+# elif mynumbers < 0:
+#     print('your number is negative')
+# else:
+#     print('blank...')
+
+
+'''PROBLEM NUMBER 4'''
+
+# leapyear = 2025
+
+# if leapyear % 400 == 0 or leapyear % 4 == 0 and leapyear % 100 != 0:
+#     print(leapyear, 'this year is a leap year')
+# else:
+#     print(leapyear, 'this year is not a leap year')
+
+
+'''PROBLEM NUMBER 5'''
+
+totalMarks = 95
+
+if totalMarks >= 90:
+    print('You got A+')
+elif totalMarks < 90 and totalMarks >= 80: 
+    print('you got A') 
+elif totalMarks < 80 and totalMarks >= 70:
+    print('you got B')
+elif totalMarks < 70 and totalMarks >= 60:
+    print('you got C')
+elif totalMarks < 60 and totalMarks >= 50:
+    print('you got D')
+else:
+    print('you have failed!')
+
+
+'''PROBLEM NUMBER 6'''
